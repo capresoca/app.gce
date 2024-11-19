76f2ea2710ecf25f5787790b0686cff54b8b315d
Gestión TIC <liderdesarrollador@capresoca.gov.co>
2024-11-18 20:04:17 -0500

    Add script db

--- backupdb/init.sql
+++ backupdb/init.sql
@@ -1,4 +1,4 @@
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'write_real_password';
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'write_real_password';
-FLUSH PRIVILEGES;
+--ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'write_real_password';
+--ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'write_real_password';
+--FLUSH PRIVILEGES;
 SELECT user, host, plugin FROM mysql.user WHERE user = 'root';
